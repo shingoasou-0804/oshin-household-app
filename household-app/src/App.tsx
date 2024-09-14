0d683e03dc8f1a30a7a680f78f323dbeb25265b2
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-09-14 20:29:11 +0900

    display home about income, expense, balance

--- household-app/src/App.tsx
+++ household-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
@@ -8,15 +8,63 @@ import AppLayout from './components/layout/AppLayout';
 import { theme } from './theme/theme';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
+import { Transaction } from './types/index';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './firebase';
+import { format } from 'date-fns';
+import { formatMonth } from './utils/formatting';
 
 function App() {
+
+  function isFireStoreError(err: unknown): err is {code: string, message: string} {
+    return typeof err === "object" && err !== null && "code" in err;
+  }
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  console.log(currentMonth);
+  const a = format(currentMonth, "yyyy-MM");
+  console.log(a);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
+        console.log(querySnapshot);
+        const transactionsData = querySnapshot.docs.map((doc) => {
+          // console.log(doc.id, " => ", doc.data());
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction
+        });
+        console.log(transactionsData);
+        setTransactions(transactionsData);
+      } catch (err) {
+        if (isFireStoreError(err)) {
+          console.error("Firestore error: ", err)
+          console.error("Firestore error messages: : ", err.message);
+          console.error("Firestore error code: ", err.code);
+        } else {
+          console.error("Genaral error: ", err);
+        }
+      }
+    }
+    fetchTransactions();
+  }, [])
+
+  const monthlyTransactions = transactions.filter((transaction) => {
+    return transaction.date.startsWith(formatMonth(currentMonth));
+  });
+
+  console.log(monthlyTransactions);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
         <Routes>
           <Route path="/" element={<AppLayout />}>
-            <Route index element={<Home />}/>
+            <Route index element={<Home monthlyTransactions={monthlyTransactions} />}/>
             <Route path="/report" element={<Report />}/>
             <Route path="*" element={<NoMatch />}/>
           </Route>
---
+++ household-app/src/types/index.ts
@@ -0,0 +1,19 @@
+export type TransactionType = 'income' | 'expense';
+export type IncomeCategory = "給与" | "副収入" | "お小遣い";
+export type ExpenseCategory = "食費" | "日用品" | "交通費" | "医療費" | "保険" | "水道光熱費" | "通信費" | "住宅" | "税金" | "教育" | "交際費" | "美容" | "趣味" | "特別費" | "その他";
+
+
+export interface Transaction {
+    id: string,
+    date: string,
+    amount: number,
+    content: string,
+    type: TransactionType,
+    category: IncomeCategory | ExpenseCategory,
+}
+
+export interface Balance {
+    income: number,
+    expense: number,
+    balance: number,
+}
