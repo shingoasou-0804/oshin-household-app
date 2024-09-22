e8138f7f25222f26a71f2cd18493131a4ffa047f
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-09-22 10:48:40 +0900

    switch income or expense categories

--- household-app/src/components/TransactionForm.tsx
+++ household-app/src/components/TransactionForm.tsx
@@ -10,8 +10,26 @@ import {
   Typography,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import FastfoodIcon from "@mui/icons-material/Fastfood";
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import TrainIcon from '@mui/icons-material/Train';
+import LocalPharmacyIcon from '@mui/icons-material/LocalPharmacy';
+import LocalAtmIcon from '@mui/icons-material/LocalAtm';
+import HotTubIcon from '@mui/icons-material/HotTub';
+import CellTowerIcon from '@mui/icons-material/CellTower';
+import HomeIcon from '@mui/icons-material/Home';
+import CurrencyYenIcon from '@mui/icons-material/CurrencyYen';
+import SchoolIcon from '@mui/icons-material/School';
+import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+import WorkIcon from '@mui/icons-material/Work';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import SavingsIcon from '@mui/icons-material/Savings';
 import { Controller, useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { ExpenseCategory, IncomeCategory } from "../types";
 
 interface TransactionFormProps {
   onCloseForm: () => void;
@@ -21,9 +39,40 @@ interface TransactionFormProps {
 
 type IncomeExpense = "income" | "expense";
 
+interface CategoryItem {
+  label: IncomeCategory | ExpenseCategory;
+  icon: JSX.Element;
+}
+
 const TransactionForm = ({ onCloseForm, isEntryDrawerOpen, currentDay }: TransactionFormProps) => {
   const formWidth = 320;
-  const { control, setValue } = useForm({
+
+  const expenseCategories: CategoryItem[] = [
+    {label: "食費", icon: <FastfoodIcon fontSize="small" />},
+    {label: "日用品", icon: <AlarmIcon fontSize="small" />},
+    {label: "交通費", icon: <TrainIcon fontSize="small" />},
+    {label: "医療費", icon: <LocalPharmacyIcon fontSize="small" />},
+    {label: "保険", icon: <LocalAtmIcon fontSize="small" />},
+    {label: "水道光熱費", icon: <HotTubIcon fontSize="small" />},
+    {label: "通信費", icon: <CellTowerIcon fontSize="small" />},
+    {label: "住宅", icon: <HomeIcon fontSize="small" />},
+    {label: "税金", icon: <CurrencyYenIcon fontSize="small" />},
+    {label: "教育", icon: <SchoolIcon fontSize="small" />},
+    {label: "交際費", icon: <DinnerDiningIcon fontSize="small" />},
+    {label: "衣料品", icon: <AccessibilityNewIcon fontSize="small" />},
+    {label: "趣味", icon: <FitnessCenterIcon fontSize="small" />},
+    {label: "その他", icon: <TipsAndUpdatesIcon />},
+  ];
+
+  const incomeCategories: CategoryItem[] = [
+    {label: "給与", icon: <WorkIcon fontSize="small" />},
+    {label: "副収入", icon: <AddBusinessIcon fontSize="small" />},
+    {label: "お小遣い", icon: <SavingsIcon fontSize="small" />},
+  ];
+
+  const [categories, setCategories] = useState(expenseCategories);
+
+  const { control, setValue, watch } = useForm({
     defaultValues: {
       type: "expense",
       date: currentDay,
@@ -35,7 +84,17 @@ const TransactionForm = ({ onCloseForm, isEntryDrawerOpen, currentDay }: Transac
 
   const incomeExpenseToggle = (type: IncomeExpense) => {
     setValue("type", type);
-  }
+  };
+
+  const currentType = watch("type");
+  useEffect(() => {
+    const newCategories = currentType === "expense" ? expenseCategories : incomeCategories;
+    setCategories(newCategories);
+  }, [currentType])
+
+  useEffect(() => {
+    setValue("date", currentDay);
+  }, [currentDay])
 
   return (
     <Box
@@ -115,12 +174,17 @@ const TransactionForm = ({ onCloseForm, isEntryDrawerOpen, currentDay }: Transac
                 label="カテゴリ"
                 select
               >
-                <MenuItem value={"食費"}>
-                  <ListItemIcon>
-                    <FastfoodIcon />
-                  </ListItemIcon>
-                  食費
-                </MenuItem>
+                {categories.map((category) => (
+                  <MenuItem
+                    key={category.label}
+                    value={category.label}
+                  >
+                    <ListItemIcon>
+                      {category.icon}
+                    </ListItemIcon>
+                    {category.label}
+                  </MenuItem>
+                ))}
               </TextField>
             )}
           />
@@ -146,7 +210,7 @@ const TransactionForm = ({ onCloseForm, isEntryDrawerOpen, currentDay }: Transac
               />
             )}
           />
-          <Button type="submit" variant="contained" color={"primary"} fullWidth>
+          <Button type="submit" variant="contained" color={currentType === "income" ? "primary" : "error" } fullWidth>
             保存
           </Button>
         </Stack>
--- household-app/src/components/common/IconComponents.tsx
+++ household-app/src/components/common/IconComponents.tsx
@@ -4,14 +4,17 @@ import SavingsIcon from '@mui/icons-material/Savings';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import AlarmIcon from '@mui/icons-material/Alarm';
 import TrainIcon from '@mui/icons-material/Train';
-import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
-import ElderlyIcon from '@mui/icons-material/Elderly';
+import LocalPharmacyIcon from '@mui/icons-material/LocalPharmacy';
+import LocalAtmIcon from '@mui/icons-material/LocalAtm';
+import HotTubIcon from '@mui/icons-material/HotTub';
+import CellTowerIcon from '@mui/icons-material/CellTower';
 import HomeIcon from '@mui/icons-material/Home';
 import CurrencyYenIcon from '@mui/icons-material/CurrencyYen';
 import SchoolIcon from '@mui/icons-material/School';
 import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
 import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
 import { ExpenseCategory, IncomeCategory } from '../../types';
 
 const IconComponents: Record<IncomeCategory | ExpenseCategory, JSX.Element> = {
@@ -21,17 +24,17 @@ const IconComponents: Record<IncomeCategory | ExpenseCategory, JSX.Element> = {
   食費: <FastfoodIcon fontSize="small" />,
   日用品: <AlarmIcon fontSize="small" />,
   交通費: <TrainIcon fontSize="small" />,
-  医療費: <ElderlyIcon fontSize="small" />,
-  保険: <LocalHospitalIcon fontSize="small" />,
-  水道光熱費: <LocalHospitalIcon fontSize="small" />,
-  通信費: <LocalHospitalIcon fontSize="small" />,
+  医療費: <LocalPharmacyIcon fontSize="small" />,
+  保険: <LocalAtmIcon fontSize="small" />,
+  水道光熱費: <HotTubIcon fontSize="small" />,
+  通信費: <CellTowerIcon fontSize="small" />,
   住宅: <HomeIcon fontSize="small" />,
   税金: <CurrencyYenIcon fontSize="small" />,
   教育: <SchoolIcon fontSize="small" />,
   交際費: <DinnerDiningIcon fontSize="small" />,
   衣料品: <AccessibilityNewIcon fontSize="small" />,
   趣味: <FitnessCenterIcon fontSize="small" />,
-  その他: <LocalHospitalIcon fontSize="small" />,
+  その他: <TipsAndUpdatesIcon fontSize="small" />,
 }
 
 export default IconComponents
