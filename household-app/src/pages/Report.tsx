9e45f55cc0fa40e595dc2956c026934f64c82159
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-10-11 09:25:06 +0900

    create layout of report display

--- household-app/src/pages/Report.tsx
+++ household-app/src/pages/Report.tsx
@@ -1,11 +1,25 @@
-import { Box } from '@mui/material'
+import { Grid, Paper } from '@mui/material'
 import React from 'react'
 
 const Report = () => {
+  const commonPaperStyle = {
+    height: { xs: "auto", md: "400px" },
+  }
   return (
-    <div>
-      <Box fontWeight={"fontWeightBold"}>Report</Box>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        日付
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Paper sx={commonPaperStyle}>カテゴリグラフ</Paper>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Paper sx={commonPaperStyle}>棒グラフ</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        テーブル
+      </Grid>
+    </Grid>
   )
 }
 
