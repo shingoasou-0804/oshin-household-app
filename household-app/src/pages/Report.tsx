d66c45ce976c6145056d015ec2eb154167d622eb
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-10-12 08:55:53 +0900

    add style to paper element and create components

--- household-app/src/pages/Report.tsx
+++ household-app/src/pages/Report.tsx
@@ -1,23 +1,33 @@
 import { Grid, Paper } from '@mui/material'
 import React from 'react'
+import MonthSelector from '../components/MonthSelector'
+import CategoryChart from '../components/CategoryChart'
+import BarChart from '../components/BarChart'
+import TransactionTable from '../components/TransactionTable'
 
 const Report = () => {
   const commonPaperStyle = {
     height: { xs: "auto", md: "400px" },
+    display: "flex",
+    flexDirection: "column",
   }
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        日付
+        <MonthSelector />
       </Grid>
       <Grid item xs={12} md={4}>
-        <Paper sx={commonPaperStyle}>カテゴリグラフ</Paper>
+        <Paper sx={commonPaperStyle}>
+          <CategoryChart />
+        </Paper>
       </Grid>
       <Grid item xs={12} md={8}>
-        <Paper sx={commonPaperStyle}>棒グラフ</Paper>
+        <Paper sx={commonPaperStyle}>
+          <BarChart />
+        </Paper>
       </Grid>
       <Grid item xs={12}>
-        テーブル
+        <TransactionTable />
       </Grid>
     </Grid>
   )
