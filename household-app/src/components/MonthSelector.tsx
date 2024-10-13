c5a49ee03849bbeab20569785a8a94eb13317750
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-10-13 14:51:28 +0900

    create selected date area

--- household-app/src/components/MonthSelector.tsx
+++ household-app/src/components/MonthSelector.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Box, Button } from '@mui/material'
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
 
 const MonthSelector = () => {
   return (
-    <div>MonthSelector</div>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Box sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <Button color={"error"} variant="contained">
+          先月
+        </Button>
+        <DatePicker sx={{ mx: 2 }} />
+        <Button color={"primary"} variant="contained">
+          次月
+        </Button>
+      </Box>
+    </LocalizationProvider>
   )
 }
 
