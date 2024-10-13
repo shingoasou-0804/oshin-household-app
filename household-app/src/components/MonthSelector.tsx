2dfb929f41518cc9dadf93787d68b1454ff35446
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-10-13 15:10:44 +0900

    convert datepicker to japanese

--- household-app/src/components/MonthSelector.tsx
+++ household-app/src/components/MonthSelector.tsx
@@ -1,15 +1,27 @@
 import { Box, Button } from '@mui/material'
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
+import { ja } from 'date-fns/locale'
 
 const MonthSelector = () => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <LocalizationProvider
+      dateAdapter={AdapterDateFns}
+      adapterLocale={ja}
+    >
       <Box sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
         <Button color={"error"} variant="contained">
           先月
         </Button>
-        <DatePicker sx={{ mx: 2 }} />
+        <DatePicker
+          label="年月を選択"
+          sx={{ mx: 2, backgroundColor: "white" }}
+          views={["year", "month"]}
+          format="yyyy/MM"
+          slotProps={{
+            calendarHeader: { format: "yyyy年MM月" },
+          }}
+        />
         <Button color={"primary"} variant="contained">
           次月
         </Button>
