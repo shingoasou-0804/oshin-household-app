d2ea79105dcc621760fd9ce77555a11f711e00b6
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-10-14 13:09:38 +0900

    add react chart js and fake data

--- household-app/src/components/BarChart.tsx
+++ household-app/src/components/BarChart.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import {
+  Chart as Chartjs,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2';
+
+Chartjs.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const BarChart = () => {
-  return (
-    <div>BarChart</div>
-  )
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: '月次収支',
+      },
+    },
+  };
+  const labels = ['2024-10-01', '2024-10-08', '2024-10-16', '2024-10-24'];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: '支出',
+        data: [65, 59, 80, 81],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: '収入',
+        data: [28, 48, 40, 19],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+  return <Bar options={options} data={data} />;
 }
 
 export default BarChart
