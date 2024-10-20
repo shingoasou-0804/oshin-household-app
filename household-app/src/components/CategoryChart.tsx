ee21599d1a6ad9e0bb378f2215836cf616eac4bc
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-10-20 13:15:04 +0900

    create a select element that switches between income and expenditure types

--- household-app/src/components/CategoryChart.tsx
+++ household-app/src/components/CategoryChart.tsx
@@ -1,9 +1,16 @@
 import { Pie } from "react-chartjs-2"
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Box, MenuItem, TextField } from "@mui/material";
+import { TransactionType } from "../types";
+import { useState } from "react";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const CategoryChart = () => {
+  const [selectedType, setSelectedType] = useState<TransactionType>("expense");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      setSelectedType(e.target.value as TransactionType);
+  }
   const data = {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [
@@ -31,8 +38,20 @@ const CategoryChart = () => {
     ],
   };
   return (
-    <Pie data={data} />
-  )
+    <Box>
+      <TextField
+        label="収支の種類"
+        select
+        fullWidth
+        value={selectedType}
+        onChange={handleChange}
+      >
+        <MenuItem value={"income"}>収入</MenuItem>
+        <MenuItem value={"expense"}>支出</MenuItem>
+      </TextField>
+      <Pie data={data} />
+    </Box>
+  );
 }
 
 export default CategoryChart
